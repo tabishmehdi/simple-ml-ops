3d2f16ed75dcad5ad7f634c2410d8d3e6cf8e53d
Noopur Tripathi <v-notrip@microsoft.com>
2021-05-21 00:26:51 +0530

    test added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    description='Its a wine q package',
+    author='Tabish',
+    packages=find_packages(),
+    license='MIT'
+)
