63933458112c3931f2220346c94c7834158aa624
Noopur Tripathi <v-notrip@microsoft.com>
2021-06-16 12:00:22 +0530

    Calculation added

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ template_dir = os.path.join(webapp_root,"templates")
 app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
 ##### Route Method
-@app.route("/",methods=["GET", "POST"])
+@app.route("/home",methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
@@ -31,6 +31,28 @@ def index():
             return render_template("404.html", error=error)
     else:
         return render_template("index.html")
+
+@app.route("/",methods=["GET", "POST"])
+def calculation():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data_req = dict(request.form)
+                mass = data_req['mass']
+                density = data_req['density']
+                diameter = data_req['diameter']
+                radius = float(diameter) / 2
+                vol = float(mass) / float(density)
+                vol_one_sphere = 4 / 3 * 3.14 * radius ** 3
+                response = vol / vol_one_sphere
+                return render_template("calculate.html", response=str(response))
+        except Exception as e:
+            print(e)
+            # error = {"error" : "Something went wrong!! Try again."}
+            error = {"error" : e}
+            return render_template("404.html", error=error)
+    else:
+        return render_template("calculate.html")
 
 
 if __name__ == "__main__":
